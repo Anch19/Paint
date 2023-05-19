21537aac2dfc0155cdc1a024abb320e1396ddee1
Antriksh chaudhary <anch00001@stud.uni-saarland.de>
2023-05-19 21:32:27 +0200

    Resize image done with calloc

--- src/image_edit.c
+++ src/image_edit.c
@@ -42,7 +42,7 @@ void rotate_clockwise(image_t *img)
             int newcolumn = img->h - 1 - i;
             pixels[newrow * img->h + newcolumn] = img->img[i * img->w + j];
         }
-        //this formula to rotate the image is been taken from chatgpt
+        // this formula to rotate the image is been taken from chatgpt
     }
 
     int swap = img->w;
@@ -52,46 +52,67 @@ void rotate_clockwise(image_t *img)
     free(img->img);
     img->img = pixels;
     // free(pixels);
-
-
 }
 
 void mirror_horizontal(image_t *img)
 {
     int width = img->w;
     int height = img->h;
-    
-    pixel_t *mirror1= malloc(width * height * (sizeof(pixel_t)));
-    for(int i = 0; i<width; ++i){    // i gives number of coloum , it is x inn slides
-        for(int j = 0 ;j<height; ++j){  // j gives the number of rows, it is y in slides
-        mirror1[(j* width) +i]= img->img[(j *width) + width-(i+1)];
 
+    pixel_t *mirror1 = malloc(width * height * (sizeof(pixel_t)));
+    for (int i = 0; i < width; ++i)
+    { // i gives number of coloum , it is x inn slides
+        for (int j = 0; j < height; ++j)
+        { // j gives the number of rows, it is y in slides
+            mirror1[(j * width) + i] = img->img[(j * width) + width - (i + 1)];
         }
     }
     free(img->img);
-    img->img= mirror1;
+    img->img = mirror1;
 }
 
 void mirror_vertical(image_t *img)
 {
     int width = img->w;
     int height = img->h;
-    
-    pixel_t *mirror2= malloc(width * height * (sizeof(pixel_t)));
-    for(int i = 0; i<width; ++i){    // i gives number of coloum , it is x inn slides
-        for(int j = 0 ;j<height; ++j){  // j gives the number of rows, it is y in slides
-        mirror2[(j* width) +i]= img->img[(width-(j+1)) *width + i];
 
+    pixel_t *mirror2 = malloc(width * height * (sizeof(pixel_t)));
+    for (int i = 0; i < width; ++i)
+    { // i gives number of coloum , it is x inn slides
+        for (int j = 0; j < height; ++j)
+        { // j gives the number of rows, it is y in slides
+            mirror2[(j * width) + i] = img->img[((width - (j + 1)) * width) + i];
         }
     }
     free(img->img);
-    img->img= mirror2;
+    img->img = mirror2;
 }
 
 void resize(image_t *img, int new_width, int new_height)
 {
-    NOT_IMPLEMENTED;
-    UNUSED(img);
-    UNUSED(new_width);
-    UNUSED(new_height);
+    
+    int oldwidth = img->w;
+    int oldheight = img->h;
+     
+     image_t *resized_image = malloc(sizeof(image_t));
+     resized_image->w = new_width;
+     resized_image->h = new_height;
+
+        resized_image->img = calloc(new_width * new_height , sizeof(pixel_t));
+
+        for (int i = 0; i < new_width; i++)
+        {
+            for (int j = 0; j < new_height; j++)
+            {
+
+                if(i<oldwidth && j <oldheight){
+                resized_image->img[(j * new_width) + i]= img->img[(j * oldwidth) + i];
+            }
+        }
+    }
+     free(img->img);
+
+    *img=*resized_image;
+    
+    
 }
